192a5742bcb9f41fc9cd33f7371e9c036378a50b
Annsofies <eaa25anss@students.eaaa.dk>
2026-04-16 13:34:42 +0200

    Opgave 3 – Opret datastrukturen movies med de første 5 film
    tilføjet de første 5 film

--- js/movies-script.js
+++ js/movies-script.js
@@ -1,1 +1,53 @@
 "use strict";
+
+// Opgave 3 – Opret datastrukturen movies med de første 5 film. 
+// Hvert objekt skal have følgende properties: id, title, genre, year, duration, img, url
+
+
+const exhibitions = [
+  {
+    id: 1,
+    titel: "Inception",
+    genre: "science-fiction",
+    year: 2010,
+    duration: 2.28,
+    img: img/inception.webp,
+    url: <a href="https://www.imdb.com/title/tt1375666/">https://www.imdb.com/title/tt1375666/</a>,
+   },
+  {
+    id: 2,
+    titel: "The Dark Knight",
+    genre: "action",
+    year: 2008,
+    duration: 2.32,
+    img: img/the-dark-knight.webp,
+    url: <a href="https://www.imdb.com/title/tt0468569/">https://www.imdb.com/title/tt0468569/</a>,
+  },
+  {
+    id: 3,
+    titel: "Forrest Gump",
+    genre: "drama",
+    year: 1994,
+    duration: 2.22,
+    img:img/forrest-gump.webp
+    url: <a href="https://www.imdb.com/title/tt0109830/">https://www.imdb.com/title/tt0109830/</a>,
+  },
+  {
+    id: 4,
+    titel: "Superbad",
+    genre: "comedy",
+    year: 2007,
+    duration: 1.53,
+    img: img/superbad.webp
+    url: <a href="https://www.imdb.com/title/tt0829482/">https://www.imdb.com/title/tt0829482/</a>,
+  },
+  {
+    id: 5,
+    titel: "It",
+    genre: "horror",
+    year: 2017,
+    duration: 2.15,
+    img: img/it.webp,
+    url: <a href="https://www.imdb.com/title/tt1396484/">https://www.imdb.com/title/tt1396484/</a>,
+  }
+];
